18fe49d84547ec905182e62f96bbd94f234b852a
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-04-03 14:32:57 -0400

    modify file structure and modify, add skeleton to Inventory Form

---
+++ phase-3-sinatra-react-project-frontend/src/InventoryForm.js
@@ -0,0 +1,13 @@
+import {React, useState} from 'react';
+
+function InventoryForm(){
+
+
+    return (
+        <form id="addInventory">
+            <h2>Add Inventory</h2>
+        </form>
+    )
+}
+
+export default InventoryForm;

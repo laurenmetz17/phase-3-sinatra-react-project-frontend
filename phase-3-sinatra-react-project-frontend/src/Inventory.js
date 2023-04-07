aa80fec8f0f3900d19714c807ac90970b85baeeb
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-04-07 13:35:15 -0400

    add shoe card file and basic render of inventory

--- phase-3-sinatra-react-project-frontend/src/Inventory.js
+++ phase-3-sinatra-react-project-frontend/src/Inventory.js
@@ -1,12 +1,18 @@
 import {React, useState, useEffect} from 'react';
+import ShoeCard from './ShoeCard';
 
 function Inventory({inventory, setInventory}) {
 
     console.log(inventory)
 
+    const inventoryItems = inventory.map(shoe => (
+        <ShoeCard key={shoe.name} shoe={shoe} />
+    ))
+
     return (
-        <h2>Inventory</h2>
+        <h2>{inventoryItems}</h2>
     )
+    
 
 }
 
---
+++ phase-3-sinatra-react-project-frontend/src/ShoeCard.js
@@ -0,0 +1,10 @@
+function ShoeCard({shoe}) {
+
+    return (
+        <h2>{shoe.name}</h2>
+    )
+
+
+}
+
+export default ShoeCard;

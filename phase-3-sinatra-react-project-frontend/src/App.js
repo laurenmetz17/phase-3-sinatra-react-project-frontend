21f25f952cd29201e2813891963695f786989123
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-04-07 13:27:53 -0400

    get fetch to database working and move up to App

--- phase-3-sinatra-react-project-frontend/src/App.js
+++ phase-3-sinatra-react-project-frontend/src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import Inventory from './Inventory';
 import InventoryForm from './InventoryForm';
 import { Route, Routes } from "react-router-dom";
 
 function App() {
 
-  const [inventory, setInventory] = useState({})
+  const [inventory, setInventory] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/inventory")
+    .then(resp => resp.json())
+    .then(data => {
+        setInventory(data)
+        console.log(data)
+    })
+  },[])
   
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header"></header>
       <InventoryForm inventory={inventory} setInventory={setInventory}/>
       <Inventory inventory={inventory} setInventory={setInventory}/>
     </div>
--- phase-3-sinatra-react-project-frontend/src/Inventory.js
+++ phase-3-sinatra-react-project-frontend/src/Inventory.js
@@ -2,13 +2,7 @@ import {React, useState, useEffect} from 'react';
 
 function Inventory({inventory, setInventory}) {
 
-    useEffect(() => {
-        fetch("http://localhost:9292/inventory").then(resp => resp.json())
-        .then(data => {
-            setInventory(data)
-            console.log(inventory)
-        })
-    },[])
+    console.log(inventory)
 
     return (
         <h2>Inventory</h2>
